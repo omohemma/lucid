0506f3907de15e44f6bb2dd3b9e7dc5af3450ecc
omohemma <emmaomoh@gmail.com>
2025-05-21 05:21:25 +0100

    refactor: configure zustand store

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import {Geist, Geist_Mono} from "next/font/google";
-import {create} from 'zustand';
+import {useFormulaStore} from "@/store";
+
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const useFormulaStore = create((set) => ({}));
 
 export default function Home() {
+    const {} = useFormulaStore();
     return (
         <div
             className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
